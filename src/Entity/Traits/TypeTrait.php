8e6f1a5ca386362ead1e6486c58a63729a8ead41
Manuel Bertrams <manuel@bertrams-media.de>
2025-10-16 22:22:24 +0200

    TypeTrait

---
+++ src/Entity/Traits/TypeTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Manuxi\SuluSharedToolsBundle\Entity\Traits;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+trait TypeTrait
+{
+
+    #[ORM\Column(type: Types::STRING, length: 255)]
+    private ?string $type = null;
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+        return $this;
+    }
+}
